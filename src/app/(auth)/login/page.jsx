daefdaf0cf8077544aebbee62e885e98cd6a72df
David Onifade <davidicfola@gmail.com>
2023-11-29 02:08:17 +0000

    initial commit

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image"
+import Link from "next/link"
+import Head from "next/head"
+import { HiOutlineLightBulb } from "react-icons/hi2";
+
 export default function Chat() {
     return (
       <main className="flex min-h-screen dark:bg-black flex-col items-center justify-between p-24">
         <h1 className='dark:text-white'>Login</h1>
+
       </main>
     )
   }
