6d0450cb0044cebcf0fee90f085f3ddbb6f5cccc
David Onifade <davidicfola@gmail.com>
2023-12-07 23:35:06 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
 
-    // Or for  `src` directory:
+    // Or for `src` directory:
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
