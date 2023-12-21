3032ddd6eca4296abde10a41b25e2da3a1d2eb05
David Onifade <davidicfola@gmail.com>
2023-12-21 21:57:09 +0000

    new

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,13 @@
+'use client'
 import React from "react";
 import Link from "next/link";
-import ThemeToggle from "./ThemeToggle";
-import { useTheme } from '../context/ThemeContext';
+import { useTheme } from "next-themes"
 
 export default function Navbar() {
-  const { theme } = useTheme();
+  const { setTheme } = useTheme()
   return (
-    <div style={{ color: theme === 'light' ? 'black' : 'white' }}>
-    <header className="h-15 w-full shadow-sm dark:border-gray-700 ">
+    <div>    
+    <header className="h-15 w-full shadow-sm dark:border-gray-700">
       <div className="container px-4 sm:px-6 py-4 flex justify-between items-center">
         {/* Logo */}
         <h2 className="font-bold text-3xl dark:text-dark">
@@ -16,6 +16,8 @@ export default function Navbar() {
         <div className="flex items-center space-x-2">
           {/* Add ThemeToggle component here */}
           {/* <ThemeToggle /> */}
+          <button onClick={() => setTheme("light")}>Light</button>
+      <button onClick={() => setTheme("dark")}>Dark</button>
         </div>
       </div>
     </header>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
-// app.tsx
-
-import { ThemeProvider } from '@/context/ThemeContext';
-import '@/styles/globals.css';
-import Navbar from '@/components/Nav';
-import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@/components/theme-provider'
+import '@/styles/globals.css'
+import Navbar from '@/components/Nav'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
